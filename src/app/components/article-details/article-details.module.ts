9d6c5c10e933f547e89b4771be23ef4a7ed19a48
gunturputprat <gppratama17@gmail.com>
2021-09-30 17:05:40 +0700

    added lazy load

--- src/app/components/article-details/article-details.module.ts
+++ src/app/components/article-details/article-details.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ArticleDetailsRoutingModule } from './article-details-routing.module';
-import { ArticleDetailsComponent } from '../article-details/article-details.component';
+import { ArticleDetailsComponent } from './article-details.component';
 
 @NgModule({
   declarations: [ArticleDetailsComponent],
